4b7b12ce794d8c5958e8d286dcb5199d1d950f8a
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2019-10-11 00:42:41 +0200

    added json and js

---
+++ script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const mainContainer = document.getElementById('main');
+const myRequest = new Request('./projects.json');
+
+function fetchMovieList(request) {
+    fetch(request)
+        .then(function(response) {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                throw new Error('Something went wrong on api server!');
+            }
+        })
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(error => {
+            console.log.error(error);
+        });
+}
+
+fetchMovieList(myRequest);
+
+function createPractice(course) {
+    const card = document.createElement('a');
+    card.setAttribute('class', 'portfolio-card');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', 'https://unsplash.it/1920/1080');
+
+    const h3 = document.createElement('h3');
+    h3.setAttribute('class', 'portfolio-card-title');
+
+    const p = document.createElement('p');
+    p.setAttribute('class', 'portfolio-card-p');
+    p.textContent = 'Project description';
+
+    card.appendChild(img);
+    card.appendChild(h3);
+    card.appendChild(p);
+
+    mainContainer.appendChild(card);
+}
