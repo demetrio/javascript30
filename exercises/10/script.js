25bc102f9cc3a25e722c6f7858b661857ddeb9a9
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2019-10-19 19:14:52 +0200

    10 finished! :)

---
+++ exercises/10/script.js
@@ -0,0 +1,27 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+let lastChecked;
+
+//console.log(checkboxes);
+
+function handleCheck(e) {
+    //console.log(e);
+
+    let inBetween = false;
+
+    if (e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            if ((checkbox === this) | (checkbox === lastChecked)) {
+                inBetween = !inBetween;
+            }
+
+            console.log(this.checked);
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        });
+    }
+    lastChecked = this;
+}
+
+//
+checkboxes.forEach(element => element.addEventListener('click', handleCheck));
