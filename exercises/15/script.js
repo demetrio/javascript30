a582a7b73e30c976becf9e0b3941f2916afdb11c
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2019-10-22 23:43:34 +0200

    15th completed with clear, checkAll and uncheckAll options!

--- exercises/15/script.js
+++ exercises/15/script.js
@@ -1,5 +1,10 @@
 const addItems = document.querySelector('.add-items'); // <form class="add-items">
 const itemsList = document.querySelector('.plates'); // <ul class="plates">
+
+const clear = document.querySelector('.clear');
+const checkAll = document.querySelector('.check-all');
+const uncheckAll = document.querySelector('.uncheck-all');
+
 const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
@@ -11,6 +16,7 @@ function addItem(e) {
         text,
         done: false,
     };
+
     items.push(item);
 
     localStorage.setItem('items', JSON.stringify(items)); // Need to be a String
@@ -33,14 +39,35 @@ function populateList(plates = [], platesList) {
 }
 
 function toggleDone(e) {
-    if (!e.target.matches('input')) return; // skip it if it isnt a input
+    if (!e.target.matches('input')) return; // skip-it if it isnt an input
 
     const el = e.target; // <li> ... </li>
     const index = el.dataset.index;
     items[index].done = !items[index].done;
 
+    updateLists(items);
+}
+
+function updateLists(items) {
     localStorage.setItem('items', JSON.stringify(items)); // Need to be a String
     populateList(items, itemsList);
+}
+
+function clearList() {
+    console.log('hello!');
+    items.length = 0;
+    updateLists(items);
+}
+
+function changeStatusItem(status) {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    checkboxes.forEach(checkbox => {
+        //console.log(checkbox.dataset.index);
+        checkbox.checked = items[checkbox.dataset.index].done = status;
+    });
+    console.log(`All the boxes has been ${status ? 'checked' : 'unchecked'}`);
+    updateLists(items);
 }
 
 // Event Delegation: search for something that its there from the beginning.
@@ -48,5 +75,9 @@ itemsList.addEventListener('click', toggleDone);
 
 // We dont listen to click, we need to listen to submit (imagine an "ENTER")
 addItems.addEventListener('submit', addItem);
+
+clear.addEventListener('click', clearList);
+checkAll.addEventListener('click', () => changeStatusItem(true));
+uncheckAll.addEventListener('click', () => changeStatusItem(false));
 
 populateList(items, itemsList);
