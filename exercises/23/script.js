c429596d8d4c6242cfe2fbbe64f9820b921f7e23
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2019-10-26 19:23:02 +0200

    23/30 done!

--- exercises/23/script.js
+++ exercises/23/script.js
@@ -4,3 +4,38 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+
+msg.text = document.querySelector('[name="text"]').value;
+
+/**
+ *  Delete the comment if you want to filter the voices
+ */
+function populateVoices() {
+    voices = this.getVoices();
+    voicesDropdown.innerHTML = voices
+        //.filter(voice => voice.lang.includes('en'))
+        .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+        .join('');
+}
+function setVoice() {
+    msg.voice = voices.find(voice => voice.name === this.value);
+    toggle();
+}
+
+function toggle(startOver = true) {
+    speechSynthesis.cancel();
+    if (startOver) {
+        speechSynthesis.speak(msg);
+    }
+}
+function setOption() {
+    console.log(this.name, this.value);
+    msg[this.name] = this.value;
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+/* Here you can do it with toggle.bind(null, false) or with () => toggle(false); */
+stopButton.addEventListener('click', () => toggle(false));
