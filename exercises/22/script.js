7afdf2c1ce313cd57252857002ac1954ee309810
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2019-10-26 19:22:49 +0200

    22/30 done!

---
+++ exercises/22/script.js
@@ -0,0 +1,25 @@
+const triggers = document.querySelectorAll('a');
+
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink() {
+    /* https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect */
+    const linkCoords = this.getBoundingClientRect();
+
+    console.log(linkCoords);
+
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX,
+    };
+
+    highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
