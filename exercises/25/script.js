a31aec69f043f42724fe34546306bbb554563d43
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2019-10-27 21:32:27 +0100

    6 to go!

---
+++ exercises/25/script.js
@@ -0,0 +1,28 @@
+const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
+
+function logText(e) {
+    console.log(this.classList.value);
+    // e.stopPropagation(); // stop bubbling!
+    // console.log(this);
+}
+
+/**
+ *https://developer.mozilla.org/en/docs/Web/API/EventTarget/addEventListener
+ */
+divs.forEach(div =>
+    div.addEventListener('click', logText, {
+        capture: false,
+        once: true,
+    })
+);
+
+button.addEventListener(
+    'click',
+    () => {
+        console.log('Click!!!');
+    },
+    {
+        once: true,
+    }
+);
